803665e12b7223b79183dbbfd17ab795d2145a71
gamezter <charronalexandre@hotmail.com>
2015-06-04 15:58:53 -0400

    added money

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 /***************************Variables**************************/
 var money = 0;
+var moneyIncrement = 1;
 var skill = 0;
 var totalSkill = 0;
-var increment = 1;
+var skillIncrement = 1;
 var auto = 0;
 var cssIndex = 0;
 /***************************Game logic*************************/
 $(document).ready(function(){
 	animate();
 	$('#codeButton').click(function(){
-		skill += increment;
-		totalSkill += increment;
+		skill += skillIncrement;
+		totalSkill += skillIncrement;
 	});
 	$('#cssButton').click(function(){
 		skill -= 200;
@@ -20,18 +21,26 @@ $(document).ready(function(){
 			document.getElementById('cssButton').disabled = true;
 		}
 	});
+	$('#lawnButton').click(function(){
+		show('money');
+		money += 10;
+	});
 });
 /****************************Update*****************************/
 function update(){
 	document.getElementById('skillValue').innerHTML = skill;
+	document.getElementById('moneyValue').innerHTML = money;
 	if(skill > 10){
-			show('skill');
+		show('skill');
 	}
 	if(skill > 100){
 		show('cssButton');
 	}
 	if(skill > 200){
 		document.getElementById('cssButton').disabled = false;
+	}
+	if(totalSkill > 300){
+		show('lawnButton');
 	}
 }
 /************************Helper Functions***********************/
@@ -58,7 +67,9 @@ function show(id){
 
 var cssEffects = [	"document.getElementById('codeButton').className = 'v1'; \
 					document.getElementById('cssButton').className = 'v1'; \
-					document.getElementById('skill').className = 'v1';",
+					document.getElementById('skill').className = 'v1'; \
+					document.getElementById('lawnButton').className = 'v1'; \
+					document.getElementById('money').className = 'v1';",
 
 					"document.getElementById('textEditor').className = 'v1';",
 				]
